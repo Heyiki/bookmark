52b20d43579da96dfe686ffc87619d01dc1d473d
huangyiqi <huangyiqi@trackingmore.org>
2022-10-28 17:14:51 +0800

    test

--- api/info.php
+++ api/info.php
@@ -1,3 +1,21 @@
 <?php
 
-phpinfo();
+class Info
+{
+    public function __construct(
+        private string $token = $_ENV['NOTION_TOKEN'] ?? '',
+        private string $databaseId = $_ENV['DATABASE_ID'] ?? '',
+        private string $method = $_GET['m'] ?? '',
+    )
+    {
+    }
+
+    public function handle()
+    {
+        var_dump($this->token);
+        var_dump($this->databaseId);
+        var_dump($this->method);
+    }
+}
+
+print_r((new Info())->handle());
